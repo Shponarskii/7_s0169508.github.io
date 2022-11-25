65fa68a79f5f6e37833796ddfef38c74e8f9ead2
Shponarskii <113299874+Shponarskii@users.noreply.github.com>
2022-11-25 19:00:21 +0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -118,3 +118,23 @@ window.addEventListener("DOMContentLoaded", function () {
     calculator2();
 
 });
+
+$(document).ready(function () {
+    $(".multiple-items").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+}); 
